ce9ea48fac95877e744762fea4c5281ca66dcb40
white-letter <121116111@qq.com>
2023-03-18 00:02:17 +0800

    feat: insert element

--- src/content.ts
+++ src/content.ts
@@ -2,21 +2,46 @@ import { getItem } from './utils'
 import { YAPI_KEY, DEFAULT_OPTIONS } from './constants'
 import type { YApiOptions } from './types'
 
+const sleep = (time = 1000) => new Promise(resolve => setTimeout(resolve, time))
+
 const isMatchUrl = (url: string, matchRe: string) => {
   return new RegExp(matchRe).test(url)
 }
 
-const start = (options: YApiOptions) => {
+const createIconElement = () => {
+  const el = document.createElement('button')
+  el.innerText = 'xxx'
+  return el
+}
+
+const createElementClick = (el: HTMLElement, onclick: () => void) => {
+  el.onclick = onclick
+}
+
+const insertQuery = () => {
+
+  const queryEl = document.querySelector('.colQuery')
+  console.log('queryEl ', queryEl)
+  const iconEl = createIconElement()
+  createElementClick(iconEl, () => {
+    console.log('click icon')
+    const tBodyEl = queryEl?.querySelector('.ant-table-tbody')
+    console.log('🚀 ~ file: content.ts:29 ~ createElementClick ~ tBodyEl:', tBodyEl)
+  })
+
+  queryEl?.insertBefore(iconEl, queryEl.firstChild)
+}
+
+const start = async (options: YApiOptions) => {
   const href =  location.href
   const matched = isMatchUrl(href, options.matchRe)
   // 没有匹配
   if (!matched) {
     return
   }
-
-  const queryEl =  document.querySelector('.colQuery')
-  console.log('queryEl ', queryEl )
-  
+    
+  await sleep()
+  insertQuery()
 }
 
 getItem(YAPI_KEY).then((options) => {
